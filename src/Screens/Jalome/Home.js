34ed3fc9bb3c85ce92c48bce36bd06a94249e316
Jalome <chjalome@gmail.com>
2021-02-02 06:27:29 +0200

    added bootsrtap layout to homepage

---
+++ src/Screens/Jalome/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+const Home = ()=>{
+    return (
+        <div className="main">
+            <div className="overlay">
+                <div className="container outter-container">
+                    <div className="row">
+                        <div className="col-12 col-lg-2 home-left">col-sm-8</div>
+                        <div className="col-12 col-lg-10 home-right">    
+                            <div className="container inner">
+                                col-sm-10
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Home
